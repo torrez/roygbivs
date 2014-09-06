ef0cab24dab30c6ae0c9cbd1cb21f4d86f15430d
Andre Torrez <andre@torrez.org>
2014-09-05 22:12:55 -0700

    Added some URLs to the front page.

--- handlers/home.py
+++ handlers/home.py
@@ -5,4 +5,4 @@ from base import BaseHandler
 
 class IndexHandler(BaseHandler):
     def get(self):
-        self.render("index.html")
+        self.render("index.html", random_string=self.random_string)
